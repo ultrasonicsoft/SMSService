b5a1a38d429f3ae3b1a676d577e22a08cdb2c93c
ultrasonicsoft <uvbrainsolutions@outlook.com>
2016-08-17 15:04:58 +0530

    GetCountries done

---
+++ SMSService/Controllers/SmsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using SMSService.Models;
+using SMSService.Providers;
+
+namespace SMSService.Controllers
+{
+    [Route("mitto/[controller]")]
+    public class SmsController : Controller
+    {
+        private readonly ICountryProvider countryProvider;
+        public SmsController(ICountryProvider countryProvider)
+        {
+            this.countryProvider = countryProvider;
+        }
+
+        [HttpGet("countries.json")]
+        public IEnumerable<Country> GetCountries()
+        {
+            return countryProvider.GetAllCountries();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SMSService/Controllers/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SMSService.Controllers
+{
+    [Route("api/[controller]")]
+    public class Test : Controller
+    {
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            return new List<string> { "jai", "ganesh" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value" + id;
+        }
+    }
+}
